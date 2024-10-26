12cd29cfe38c65d25d01927701565468032d9753
Gerard Siles <gerardsiles@gmail.com>
2024-10-26 12:23:17 +0000

    initial commit

---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+import { Repository } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { CreateLessonInput } from './lesson.input';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async getLesson(id: string): Promise<Lesson> {
+    return this.lessonRepository.findOne({
+      where: { id },
+    });
+  }
+
+  async getLessons(): Promise<Lesson[]> {
+    return this.lessonRepository.find();
+  }
+
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      ...createLessonInput,
+    });
+
+    return this.lessonRepository.save(lesson);
+  }
+
+  async assignStudentsToLesson(
+    lessonId: string,
+    studentIds: string[],
+  ): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOne({
+      where: { id: lessonId },
+    });
+    const { students } = lesson;
+    lesson.students = [...(students ?? []), ...studentIds];
+
+    return this.lessonRepository.save(lesson);
+  }
+}
---
+++ src/lesson/lesson.type.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { StudentType } from 'src/student/student.type';
+
+@ObjectType('Lesson')
+export class LessonType {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  @Field((type) => ID)
+  id: string;
+
+  @Field()
+  name: string;
+
+  @Field()
+  startDate: string;
+
+  @Field()
+  endDate: string;
+
+  @Field((type) => [StudentType])
+  students: string[];
+}
